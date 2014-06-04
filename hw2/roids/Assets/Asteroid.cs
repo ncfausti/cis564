6e5115d8b84395462ec09a9dfc3564929d7c20ee
Nicholas Fausti <nick@Nicholass-MacBook-Pro.local>
2014-06-03 22:22:47 -0400

    step 30

--- hw2/roids/Assets/Asteroid.cs
+++ hw2/roids/Assets/Asteroid.cs
@@ -3,20 +3,38 @@ using System.Collections;
 
 public class Asteroid : MonoBehaviour {
 
+	public GameObject deathExplosion;
+	public int pointValue;
 	// Use this for initialization
 	void Start () {
-	
+		pointValue = 10;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
 	public void Die(){
 		// Destroy removes the gameObject from the scene 
 		// and marks it for garbage collection
 
+		// Debug.Log("OH NO I AM DYING");
+
+		/* all of Shuriken's particle effects by default use 
+		 * the convention of Z being upwards, and XY being the 
+		 * horizontal plane. as a result, since we are looking 
+		 * down the Y axis, we rotate the particle system so 
+		 * that it flys in the right way.
+		 */
+
+		Instantiate (deathExplosion, gameObject.transform.position, 
+		             Quaternion.AngleAxis (-90, Vector3.right) );
+
+		GameObject obj = GameObject.Find("GlobalObject");
+		Global g = obj.GetComponent<Global>();
+		g.score += pointValue;
+		
 		Destroy (gameObject);
 
 	}
---
+++ hw2/roids/Assets/ScoreUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreUI : MonoBehaviour {
+
+	Global globalObj;
+	GUIText scoreText;
+
+	// Use this for initialization
+	void Start () {
+		GameObject g = GameObject.Find ("GlobalObject");
+		globalObj = g.GetComponent< Global >(); 
+		int lastScore = 0;
+		scoreText = gameObject.GetComponent<GUIText>();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		scoreText.text = globalObj.score.ToString();
+	}
+}
